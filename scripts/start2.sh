b179f7b258a536cad30e0c469732d1365b4a79d5
lucknutlealucky <52352828+lucknutlealucky@users.noreply.github.com>
2022-03-25 14:54:08 +0700

    Update start2.sh

--- scripts/start2.sh
+++ scripts/start2.sh
@@ -1,67 +1,49 @@
 #!/bin/sh
 
 if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
+  mkdir -p /run/mysqld
 fi
 
-if [ -d /var/lib/mysql/mysql ]; then
-	echo '[i] MySQL directory already present, skipping creation'
+if [ -d /app/mysql ]; then
+  echo "[i] MySQL directory already present, skipping creation"
 else
-	echo "[i] MySQL data directory not found, creating initial DBs"
+  echo "[i] MySQL data directory not found, creating initial DBs"
 
-	chown -R mysql:mysql /var/lib/mysql
+  mysql_install_db --user=root > /dev/null
 
-	# Initializing database, mysql_install_db --user=mysql > /dev/null
-	mysql_install_db --user=mysql --verbose=1 --basedir=/usr --datadir=/var/lib/mysql --rpm > /dev/null
-	echo '[i] Database initialized'
-	echo "[i] MySql root password: $MYSQL_ROOT_PASSWORD"
+  if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
+    MYSQL_ROOT_PASSWORD=111111
+    echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
+  fi
 
-	# create temp file
-	tfile=`mktemp`
-	if [ ! -f "$tfile" ]; then
-	    return 1
-	fi
+  MYSQL_DATABASE=${MYSQL_DATABASE:-""}
+  MYSQL_USER=${MYSQL_USER:-""}
+  MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
 
-	# save sql
-	echo "[i] Create temp file: $tfile"
-	cat << EOF > $tfile
--- SET @@SESSION.sql_mode = 'ANSI_QUOTES';
-SET @@SESSION.sql_mode = '';
+  tfile=`mktemp`
+  if [ ! -f "$tfile" ]; then
+      return 1
+  fi
+
+  cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-GRANT SELECT, SHOW VIEW, PROCESS ON *.* TO '$MYSQL_USER_MONITORING'@'%' IDENTIFIED BY '$MYSQL_PASSWORD_MONITORING' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '';
 EOF
 
+  if [ "$MYSQL_DATABASE" != "" ]; then
+    echo "[i] Creating database: $MYSQL_DATABASE"
+    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
 
-	# Create new database
-	if [ "$MYSQL_DATABASE" != "" ]; then
-		echo "[i] Creating database: $MYSQL_DATABASE"
-		echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+    if [ "$MYSQL_USER" != "" ]; then
+      echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
+      echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+    fi
+  fi
 
-		# set new User and Password
-		if [ "$MYSQL_USER" != "" ] && [ "$MYSQL_PASSWORD" != "" ]; then
-		echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-		echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
-		fi
-	else
-		# don`t need to create new database,Set new User to control all database.
-		if [ "$MYSQL_USER" != "" ] && [ "$MYSQL_PASSWORD" != "" ]; then
-		echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-		echo "GRANT ALL ON *.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
-		fi
-	fi
-
-	echo 'FLUSH PRIVILEGES;' >> $tfile
-	
-	echo $tfile
-
-	# run sql in tempfile
-	echo "[i] run tempfile: $tfile"
-	/usr/bin/mysqld --user=mysql --verbose=1 --bootstrap < $tfile
-	rm -f $tfile
+  /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
+  rm -f $tfile
 fi
 
-echo "[i] Sleeping 3 sec"
-sleep 3
